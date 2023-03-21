a41b0d11f5cd187a9169c02b838129541ac19860
ehangehang <rayhansuryatama@gmail.com>
2023-03-21 17:54:53 +0700

    init

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Mangkir App will be developed here! Just wait 🥶
         </p>
         <a
           className="App-link"
