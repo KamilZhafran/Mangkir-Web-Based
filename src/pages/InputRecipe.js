efa780e1bc169dd883f9155ca87a987bc3116370
kamil zhafran <zhafrankamil17@gmail.com>
2023-04-08 02:17:29 +0700

    input recipe page

--- src/pages/InputRecipe.js
+++ src/pages/InputRecipe.js
@@ -14,21 +14,9 @@ export class InputRecipe extends React.Component {
         return (
             <div className={styles.bodyRecipe}>
                 <div className={styles.inRecipeTitle}>
-                    <label for="nameTitle" className={styles.formLabel}>Title</label>
+                    <label for="nameTitle" className={styles.formLabelTitle}>Title</label>
                     <input type="text" className={styles.formControl} id="nameTitle"/>
                 </div>   
-                <div className={styles.inRecipeContainer}> 
-                    <div className={`header ${styles.header}`}> 
-                        <svg className={styles.headerSvg} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> 
-                            <path d="M7 10V9C7 6.23858 9.23858 4 12 4C14.7614 4 17 6.23858 17 9V10C19.2091 10 21 11.7909 21 14C21 15.4806 20.1956 16.8084 19 17.5M7 10C4.79086 10 3 11.7909 3 14C3 15.4806 3.8044 16.8084 5 17.5M7 10C7.43285 10 7.84965 10.0688 8.24006 10.1959M12 12V21M12 12L15 15M12 12L9 15" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg> <p>Upload Food Photo!</p>
-                    </div> 
-                    <label for="file" className={`footer ${styles.footer}`}> 
-                        <svg className={styles.footerSvg} fill="#000000" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M15.331 6H8.5v20h15V14.154h-8.169z"></path><path d="M18.153 6h-.009v5.342H23.5v-.002z"></path></g></svg> 
-                        <p className={styles.footerP}>Here!</p> 
-                        <svg className={styles.footerSvg} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M5.16565 10.1534C5.07629 8.99181 5.99473 8 7.15975 8H16.8402C18.0053 8 18.9237 8.9918 18.8344 10.1534L18.142 19.1534C18.0619 20.1954 17.193 21 16.1479 21H7.85206C6.80699 21 5.93811 20.1954 5.85795 19.1534L5.16565 10.1534Z" stroke="#000000" stroke-width="2"></path> <path d="M19.5 5H4.5" stroke="#000000" stroke-width="2" stroke-linecap="round"></path> <path d="M10 3C10 2.44772 10.4477 2 11 2H13C13.5523 2 14 2.44772 14 3V5H10V3Z" stroke="#000000" stroke-width="2"></path> </g></svg>
-                    </label> 
-                    <input id="file" type="file"/> 
-                </div>
                 <div className={styles.inRecipeIngredients}>
                     <label for="exampleFormControlTextarea1" className={styles.formLabel}>Ingredients</label>
                     <textarea type="text" className={`form-control ${styles.formControl}`} id="nameTitle" rows="5"/>
@@ -37,17 +25,13 @@ export class InputRecipe extends React.Component {
                     <label for="stepsList" className={styles.formLabel}>Steps</label>
                     <textarea type="text" className={`form-control ${styles.formControl}`} id="nameTitle" rows="5"/>
                 </div>
-                <div className={styles.inRecipeContainer}> 
-                    <div className={styles.header}> 
-                    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> 
-                        <path d="M7 10V9C7 6.23858 9.23858 4 12 4C14.7614 4 17 6.23858 17 9V10C19.2091 10 21 11.7909 21 14C21 15.4806 20.1956 16.8084 19 17.5M7 10C4.79086 10 3 11.7909 3 14C3 15.4806 3.8044 16.8084 5 17.5M7 10C7.43285 10 7.84965 10.0688 8.24006 10.1959M12 12V21M12 12L15 15M12 12L9 15" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg> <p>Upload Food Video!</p>
-                    </div> 
-                    <label for="file" className={styles.footer}> 
-                    <svg fill="#000000" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M15.331 6H8.5v20h15V14.154h-8.169z"></path><path d="M18.153 6h-.009v5.342H23.5v-.002z"></path></g></svg> 
-                    <p>Here!</p> 
-                    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M5.16565 10.1534C5.07629 8.99181 5.99473 8 7.15975 8H16.8402C18.0053 8 18.9237 8.9918 18.8344 10.1534L18.142 19.1534C18.0619 20.1954 17.193 21 16.1479 21H7.85206C6.80699 21 5.93811 20.1954 5.85795 19.1534L5.16565 10.1534Z" stroke="#000000" stroke-width="2"></path> <path d="M19.5 5H4.5" stroke="#000000" stroke-width="2" stroke-linecap="round"></path> <path d="M10 3C10 2.44772 10.4477 2 11 2H13C13.5523 2 14 2.44772 14 3V5H10V3Z" stroke="#000000" stroke-width="2"></path> </g></svg>
-                    </label> 
-                    <input id="file" type="file"/> 
+                <div className={styles.inRecipeUpload}>
+                    <label for="formFileLg" className={`form-label ${styles.formLabel}`}>Input Your Food Photo</label>
+                    <input className={`form-control${styles.formControl} form-control-lg${styles.formControlLg}`} id="formFileLg" type="file"/>
+                </div><br/>
+                <div className={styles.inRecipeUpload}>
+                    <label for="formFileLg" className={`form-label ${styles.formLabel}`}>Input Your Food Video</label>
+                    <input className={`form-control${styles.formControl} form-control-lg${styles.formControlLg}`} id="formFileLg" type="file"/>
                 </div><br/>
                 <div className="d-grid gap-2 col-6 mx-auto">
                     <button type="button" className="btn btn-outline-info">Publish</button>
