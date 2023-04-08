46390f28fd45175695fdb0aac554e1dd1c028cbb
ehangehang <rayhansuryatama@gmail.com>
2023-04-08 22:48:29 +0700

    fix login register logic

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,6 +10,53 @@ export class Login extends React.Component {
         super(props);
     }
 
+    state = {
+        name: '',
+        email: '',
+        password: '',
+      };
+    
+    handleEmailChange = (event) => {
+    this.setState({ email: event.target.value });
+    };
+
+    handlePasswordChange = (event) => {
+    this.setState({ password: event.target.value });
+    };
+
+    handleLogin = async (event) => {
+        event.preventDefault();
+    
+        const loginData = {
+          email: this.state.email,
+          password: this.state.password,
+        };
+        console.log(loginData);
+    
+        try {
+          const response = await fetch('http://127.0.0.1:8000/api/login', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(loginData),
+          });
+    
+          const responseData = await response.json();
+          localStorage.setItem('access_token', responseData.token);
+          localStorage.setItem('loggedInEmail', responseData.email);
+          if (responseData.token){
+            window.location.href = '/';
+          }else{
+            console.log("login gagal");
+          }
+          // You can handle the response data here
+        } catch (error) {
+          console.log(error);
+          // You can handle the error response data here
+        }
+      };
+
     render() {
         return (
             <div className={`wrapper ${styles.inLoginWrapper}`}>
@@ -19,23 +66,25 @@ export class Login extends React.Component {
                             
                         </div>
                         <div class={`col-md-6 right ${styles.right}`}>
-                            <div className={`input-box ${styles.inputBox}`}>
-                                <header>Login</header>
-                                <div className={`input-field ${styles.inputField}`}>
-                                    <input type="text" className={`input ${styles.input}`} id="email" required autocomplete="off"/>
-                                    <label for="email">Email</label>
+                            <form action="#" onSubmit={this.handleLogin}>
+                                <div className={`input-box ${styles.inputBox}`}>
+                                    <header>Login</header>
+                                    <div className={`input-field ${styles.inputField}`}>
+                                        <input type="text" className={`input ${styles.input}`} id="email" required autocomplete="off" onChange={this.handleEmailChange}/>
+                                        <label for="email">Email</label>
+                                    </div>
+                                    <div className={`input-field ${styles.inputField}`}>
+                                        <input type="password" className={`input ${styles.input}`} id="password" required onChange={this.handlePasswordChange}/>
+                                        <label for="password">Password</label>
+                                    </div>
+                                    <div class={`input-field ${styles.inputField}`}>
+                                        <input type="submit" className={`submit ${styles.submit}`} value="Log In"/>                                   
+                                    </div>
+                                    <div className={`signin ${styles.signin}`}>
+                                        <span>Haven't got any account yet? <Link to="/register"> Register Here!</Link></span>
+                                    </div>
                                 </div>
-                                <div className={`input-field ${styles.inputField}`}>
-                                    <input type="password" className={`input ${styles.input}`} id="password" required/>
-                                    <label for="password">Password</label>
-                                </div>
-                                <div class={`input-field ${styles.inputField}`}>
-                                    <input type="submit" className={`submit ${styles.submit}`} value="Sign Up"/>                                   
-                                </div>
-                                <div className={`signin ${styles.signin}`}>
-                                    <span>Haven't got any account yet? <Link to="/register"> Register Here!</Link></span>
-                                </div>
-                            </div>
+                            </form>
                         </div>
                     </div>
                 </div>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -10,6 +10,42 @@ export class Register extends React.Component {
         super(props);
     }
 
+    state = {
+        name: '',
+        email: '',
+        password: '',
+      };
+
+    handleEmailChange = (event) => {
+    this.setState({ email: event.target.value });
+    };
+
+    handlePasswordChange = (event) => {
+    this.setState({ password: event.target.value });
+    };
+
+    handleNameChange = (event) => {
+    this.setState({ name: event.target.value });
+    };
+
+    handleRegister = async (event) => {
+        event.preventDefault();
+        console.log(this.state.name, this.state.email, this.state.password);
+        const response = await fetch('http://127.0.0.1:8000/api/register', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password,
+          }),
+        });
+        const data = await response.json();
+        console.log(data);
+      };
+
     render() {
         return (
             <div className={`wrapper ${styles.inLoginWrapper}`}>
@@ -21,17 +57,23 @@ export class Register extends React.Component {
                         <div class={`col-md-6 right ${styles.right}`}>
                             <div className={`input-box ${styles.inputBox}`}>
                                 <header>Create account</header>
-                                <div className={`input-field ${styles.inputField}`}>
-                                    <input type="text" className={`input ${styles.input}`} id="email" required autocomplete="off"/>
-                                    <label for="email">Email</label>
-                                </div>
-                                <div className={`input-field ${styles.inputField}`}>
-                                    <input type="password" className={`input ${styles.input}`} id="password" required/>
-                                    <label for="password">Password</label>
-                                </div>
-                                <div class={`input-field ${styles.inputField}`}>
-                                    <input type="submit" className={`submit ${styles.submit}`} value="Sign Up"/>                                   
-                                </div>
+                                <form action="#" onSubmit={this.handleRegister}>
+                                    <div className={`input-field ${styles.inputField}`}>
+                                        <input type="text" className={`input ${styles.input}`} id="email" required autocomplete="off" onChange={this.handleNameChange}/>
+                                        <label for="email">Name</label>
+                                    </div>
+                                    <div className={`input-field ${styles.inputField}`}>
+                                        <input type="text" className={`input ${styles.input}`} id="email" required autocomplete="off" onChange={this.handleEmailChange}/>
+                                        <label for="email">Email</label>
+                                    </div>
+                                    <div className={`input-field ${styles.inputField}`}>
+                                        <input type="password" className={`input ${styles.input}`} id="password" required onChange={this.handlePasswordChange}/>
+                                        <label for="password">Password</label>
+                                    </div>
+                                    <div class={`input-field ${styles.inputField}`}>
+                                        <input type="submit" className={`submit ${styles.submit}`} value="Sign Up"/>                                   
+                                    </div>
+                                </form>
                                 <div className={`signin ${styles.signin}`}>
                                     <span>Already have an account? <Link to="/login">Log in here</Link></span>
                                 </div>
